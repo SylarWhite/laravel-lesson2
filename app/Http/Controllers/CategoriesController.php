58cd5735c37c57d1cafd50bf73d9c23e3d03b230
Sylar <redpeachj@gmail.com>
2019-01-22 06:34:42 +0000

    分类话题列表

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    //
+    public function show(Category $category)
+    {
+        $topics = Topic::where('category_id',$category->id)->paginate(6);
+
+        return view('topics.index',compact('topics','category'));
+    }
+
+
+}
