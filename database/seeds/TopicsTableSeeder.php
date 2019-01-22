8ec8b618641dc8df4f441bbd944b5b7ee5e99a35
Sylar <redpeachj@gmail.com>
2019-01-22 02:55:40 +0000

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
 
@@ -7,10 +8,20 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        // ID 用户组
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 分类ID
+        $categories_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+        // 获取实例
+        $faker = app(Faker\Generator::class);
+
+
+
+        $topics = factory(Topic::class)->times(100)->make()->each(function ($topic, $index) use ($user_ids,$categories_ids,$faker) {
+            $topic->user_id = $faker->randomElement($user_ids);
+            $topic->category_id = $faker->randomElement($categories_ids);
         });
 
         Topic::insert($topics->toArray());
