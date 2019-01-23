1236ad8e4895d471962822eee3dc305eedf647b5
Sylar <redpeachj@gmail.com>
2019-01-23 10:51:13 +0000

    用户可用编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -41,7 +41,9 @@
                     <select class="form-control" name="category_id" required>
                       <option value="" hidden disabled selected>请选择分类</option>
                       @foreach ($categories as $value)
-                        <option value="{{ $value->id }}">{{ $value->name }}</option>
+                        <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
+                          {{ $value->name }}
+                        </option>
                       @endforeach
                     </select>
                   </div>
@@ -62,26 +64,26 @@
 @endsection
 
 @section('scripts')
-  {{--<script type="text/javascript" src="{{ asset('js/module.min.js') }}"></script>--}}
-  {{--<script type="text/javascript" src="{{ asset('js/hotkeys.min.js') }}"></script>--}}
-  {{--<script type="text/javascript" src="{{ asset('js/uploader.min.js') }}"></script>--}}
-  {{--<script type="text/javascript" src="{{ asset('js/simditor.min.js') }}"></script>--}}
+  <script type="text/javascript" src="{{ asset('js/module.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.min.js') }}"></script>
 
-  {{--<script type="text/javascript">--}}
-    {{--$(document).ready(function () {--}}
-      {{--var editor = new Simditor({--}}
-        {{--textarea: $("#editor"),--}}
-        {{--upload:{--}}
-          {{--url: '{{ route('topics.upload_image') }}',--}}
-          {{--params: {--}}
-            {{--_token: '{{ csrf_token() }}'--}}
-          {{--},--}}
-          {{--fileKey: 'upload_file',--}}
-          {{--connectionCount:3,--}}
-          {{--leaveConfirm: '文件上传至，关闭此页将取消上传。'--}}
-        {{--},--}}
-        {{--pasteImage: true,--}}
-      {{--});--}}
-    {{--});--}}
-  {{--</script>--}}
+  <script type="text/javascript">
+    $(document).ready(function () {
+      var editor = new Simditor({
+        textarea: $("#editor"),
+        upload:{
+          url: '{{ route('topics.upload_image') }}',
+          params: {
+            _token: '{{ csrf_token() }}'
+          },
+          fileKey: 'upload_file',
+          connectionCount:3,
+          leaveConfirm: '文件上传至，关闭此页将取消上传。'
+        },
+        pasteImage: true,
+      });
+    });
+  </script>
 @endsection
