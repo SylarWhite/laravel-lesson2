0a13b6c3e04d50ad5d5ce587e569359aa45007ca
Sylar <redpeachj@gmail.com>
2019-01-29 08:03:57 +0000

    修复redis 不能用

--- resources/views/pages/close_register.blade.php
+++ resources/views/pages/close_register.blade.php
@@ -12,9 +12,7 @@
         @else
           <div class="alert alert-danger text-center">
             已关闭注册。 联系QQ {{ env('QQ',123456) }} 获得邀请
-            <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2::53" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>
           </div>
-
         @endif
       </div>
     </div>
